058f7d2edd972481a2cc91fce0c92335ff9d0dd1
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2020-04-22 11:26:09 +0200

    Reformat
    M	tools/fm-viewer/FeatureModelViewer.cpp

--- tools/fm-viewer/FeatureModelViewer.cpp
+++ tools/fm-viewer/FeatureModelViewer.cpp
@@ -1,6 +1,6 @@
 #include "vara/Feature/XmlParser.h"
 
-int main(/*int argc, char ** argv*/) {
+int main(/*int argc, char **argv*/) {
   auto P = vara::feature::XmlParser(
       "../../../vara/external/vara-feature/test/test.xml",
       "../../../vara/external/vara-feature/vm.dtd");
