9b72a223226337964d9c7e44de9a08722b03f69a
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2020-04-22 12:04:12 +0200

    Fix cmake
    M	CMakeLists.txt
    M	tools/fm-viewer/FeatureModelViewer.cpp

--- tools/fm-viewer/FeatureModelViewer.cpp
+++ tools/fm-viewer/FeatureModelViewer.cpp
@@ -1,9 +1,7 @@
 #include "vara/Feature/XmlParser.h"
 
 int main(/*int argc, char **argv*/) {
-  auto P = vara::feature::XmlParser(
-      "../../../vara/external/vara-feature/test/test.xml",
-      "../../../vara/external/vara-feature/vm.dtd");
+  auto P = vara::feature::XmlParser("../test/test.xml", "../vm.dtd");
   if (P.parse()) {
     std::unique_ptr<vara::feature::FeatureModel> FM = P.buildFeatureModel();
     FM->dump();
