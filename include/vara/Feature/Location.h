fa03d291795463eb5bbd42ae9cfeba605c0301db
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2020-05-18 17:02:52 +0200

    Use experimental
    M	include/vara/Feature/Location.h

--- include/vara/Feature/Location.h
+++ include/vara/Feature/Location.h
@@ -1,12 +1,12 @@
 #ifndef VARA_FEATURE_LOCATION_H
 #define VARA_FEATURE_LOCATION_H
 
-#include <filesystem>
+#include <experimental/filesystem> // TODO (s9latimm): remove experimental
 #include <optional>
 #include <sstream>
 #include <string>
 
-namespace fs = std::filesystem;
+namespace fs = std::experimental::filesystem;
 
 namespace vara::feature {
 
