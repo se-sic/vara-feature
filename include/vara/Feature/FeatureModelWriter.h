a34b159bb0d39a3bfa50191a2215dbf85be6f815
Pascal <pascal@yag.de>
2020-07-21 11:17:46 +0200

    start

---
+++ include/vara/Feature/FeatureModelWriter.h
@@ -0,0 +1,130 @@
+#ifndef VARA_FEATURE_FEATUREMODELPARSER_H
+#define VARA_FEATURE_FEATUREMODELPARSER_H
+
+#include "vara/Feature/FeatureModel.h"
+
+#include "libxml/parser.h"
+#include "libxml/tree.h"
+
+#include <memory>
+
+namespace vara::feature {
+
+//===----------------------------------------------------------------------===//
+//                               FeatureModelParser Class
+//===----------------------------------------------------------------------===//
+
+/// \brief Base class for parsers with different input formats.
+class FeatureModelParser {
+protected:
+  explicit FeatureModelParser() = default;
+
+public:
+  virtual ~FeatureModelParser() = default;
+
+  /// Build \a FeatureModel after parsing. May return null if parsing or
+  /// building failed.
+  ///
+  /// \returns an instance of \a FeatureModel or \a nullptr
+  virtual std::unique_ptr<FeatureModel> buildFeatureModel() = 0;
+
+  /// Checks whether input is a valid feature model as acyclic graph with unique
+  /// nodes and tree like structure. Tests precondition of \a buildFeatureModel.
+  virtual bool verifyFeatureModel() = 0;
+};
+
+//===----------------------------------------------------------------------===//
+//                               FeatureModelXmlParser Class
+//===----------------------------------------------------------------------===//
+
+/// \brief Parsers for feature models in XML.
+class FeatureModelXmlParser : public FeatureModelParser {
+public:
+  explicit FeatureModelXmlParser(std::string Xml) : Xml(std::move(Xml)) {}
+
+  std::unique_ptr<FeatureModel> buildFeatureModel() override;
+
+  bool verifyFeatureModel() override;
+
+private:
+  using constXmlCharPtr = const xmlChar *;
+
+  inline static const std::string DtdRaw =
+      "<!ELEMENT vm (binaryOptions, numericOptions?, booleanConstraints?, "
+      "nonBooleanConstraints?, mixedConstraints?)>\n"
+      "<!ATTLIST vm name CDATA #REQUIRED root CDATA #IMPLIED>\n"
+      "<!ELEMENT binaryOptions (configurationOption*)>\n"
+      "<!ELEMENT numericOptions (configurationOption*)>\n"
+      "<!ELEMENT booleanConstraints (constraint*)>\n"
+      "<!ELEMENT nonBooleanConstraints (constraint*)>\n"
+      "<!ELEMENT mixedConstraints (constraint*)>\n"
+      "<!ELEMENT configurationOption (name, outputString?, (prefix | preFix)?, "
+      "(postfix | postFix)?, parent?, children?, impliedOptions?, "
+      "excludedOptions?, defaultValue?, optional?, ((minValue, maxValue) | "
+      "values)?, "
+      "stepFunction?, location?)>\n"
+      "<!ELEMENT constraint (#PCDATA)>\n"
+      "<!ATTLIST constraint req CDATA #IMPLIED exprKind CDATA #IMPLIED>\n"
+      "<!ELEMENT name (#PCDATA)>\n"
+      "<!ELEMENT outputString (#PCDATA)>\n"
+      "<!ELEMENT prefix (#PCDATA)>\n"
+      "<!ELEMENT preFix (#PCDATA)>\n"
+      "<!ELEMENT postfix (#PCDATA)>\n"
+      "<!ELEMENT postFix (#PCDATA)>\n"
+      "<!ELEMENT parent (#PCDATA)>\n"
+      "<!ELEMENT children (options*)>\n"
+      "<!ELEMENT impliedOptions (options*)>\n"
+      "<!ELEMENT excludedOptions (options*)>\n"
+      "<!ELEMENT defaultValue (#PCDATA)>\n"
+      "<!ELEMENT options (#PCDATA)>\n"
+      "<!ELEMENT optional (#PCDATA)>\n"
+      "<!ELEMENT minValue (#PCDATA)>\n"
+      "<!ELEMENT maxValue (#PCDATA)>\n"
+      "<!ELEMENT values (#PCDATA)>\n"
+      "<!ELEMENT stepFunction (#PCDATA)>\n"
+      "<!ELEMENT location (path, start?, end?)>\n"
+      "<!ELEMENT path (#PCDATA)>\n"
+      "<!ELEMENT start (line, column)>\n"
+      "<!ELEMENT end (line, column)>\n"
+      "<!ELEMENT line (#PCDATA)>\n"
+      "<!ELEMENT column (#PCDATA)>";
+
+  static constexpr xmlChar NAME[] = "name";
+  static constexpr xmlChar OPTIONAL[] = "optional";
+  static constexpr xmlChar PARENT[] = "parent";
+  static constexpr xmlChar EXCLUDEDOPTIONS[] = "excludedOptions";
+  static constexpr xmlChar OPTIONS[] = "options";
+  static constexpr xmlChar LOCATION[] = "location";
+  static constexpr xmlChar PATH[] = "path";
+  static constexpr xmlChar START[] = "start";
+  static constexpr xmlChar END[] = "end";
+  static constexpr xmlChar MINVALUE[] = "minValue";
+  static constexpr xmlChar MAXVALUE[] = "maxValue";
+  static constexpr xmlChar VALUES[] = "values";
+  static constexpr xmlChar CONFIGURATIONOPTION[] = "configurationOption";
+  static constexpr xmlChar CONSTRAINT[] = "constraint";
+  static constexpr xmlChar ROOT[] = "root";
+  static constexpr xmlChar BINARYOPTIONS[] = "binaryOptions";
+  static constexpr xmlChar NUMERICOPTIONS[] = "numericOptions";
+  static constexpr xmlChar BOOLEANCONSTRAINTS[] = "booleanConstraints";
+  static constexpr xmlChar LINE[] = "line";
+  static constexpr xmlChar COLUMN[] = "column";
+
+  std::string Xml;
+  FeatureModelBuilder FMB;
+
+  bool parseConfigurationOption(xmlNode *Node, bool Num);
+  bool parseOptions(xmlNode *Node, bool Num);
+  bool parseConstraints(xmlNode *Node);
+  bool parseVm(xmlNode *Node);
+
+  static FeatureSourceRange::FeatureSourceLocation
+  createFeatureSourceLocation(xmlNode *Node);
+
+  std::unique_ptr<xmlDoc, void (*)(xmlDocPtr)> parseDoc();
+  static std::unique_ptr<xmlDtd, void (*)(xmlDtdPtr)> createDtd();
+};
+
+} // namespace vara::feature
+
+#endif // VARA_FEATURE_FEATUREMODELPARSER_H
---
+++ lib/Feature/FeatureModelWriter.cpp
@@ -0,0 +1,226 @@
+#include "vara/Feature/FeatureModelParser.h"
+
+#include <iostream>
+#include <regex>
+
+namespace vara::feature {
+
+bool FeatureModelXmlParser::parseConfigurationOption(xmlNode *Node,
+                                                     bool Num = false) {
+  string Name;
+  bool Opt = false;
+  int MinValue = 0;
+  int MaxValue = 0;
+  std::vector<int> Values;
+  std::optional<FeatureSourceRange> Loc;
+  for (xmlNode *Head = Node->children; Head; Head = Head->next) {
+    if (Head->type == XML_ELEMENT_NODE) {
+      string Cnt = std::string(
+          reinterpret_cast<char *>(std::unique_ptr<xmlChar, void (*)(void *)>(
+                                       xmlNodeGetContent(Head), xmlFree)
+                                       .get()));
+      if (!xmlStrcmp(Head->name, NAME)) {
+        Name = Cnt;
+      } else if (!xmlStrcmp(Head->name, OPTIONAL)) {
+        Opt = Cnt == "True";
+      } else if (!xmlStrcmp(Head->name, PARENT)) {
+        FMB.addParent(Name, Cnt);
+      } else if (!xmlStrcmp(Head->name, EXCLUDEDOPTIONS)) {
+        for (xmlNode *Child = Head->children; Child; Child = Child->next) {
+          if (Child->type == XML_ELEMENT_NODE) {
+            if (!xmlStrcmp(Child->name, OPTIONS)) {
+              std::unique_ptr<xmlChar, void (*)(void *)> CCnt(
+                  xmlNodeGetContent(Child), xmlFree);
+              FMB.addExclude(Name, reinterpret_cast<char *>(CCnt.get()));
+            }
+          }
+        }
+      } else if (!xmlStrcmp(Head->name, LOCATION)) {
+        fs::path Path;
+        std::optional<FeatureSourceRange::FeatureSourceLocation> Start;
+        std::optional<FeatureSourceRange::FeatureSourceLocation> End;
+        for (xmlNode *Child = Head->children; Child; Child = Child->next) {
+          if (Child->type == XML_ELEMENT_NODE) {
+            if (!xmlStrcmp(Child->name, PATH)) {
+              Path = fs::path(reinterpret_cast<char *>(
+                  std::unique_ptr<xmlChar, void (*)(void *)>(
+                      xmlNodeGetContent(Child), xmlFree)
+                      .get()));
+
+            } else if (!xmlStrcmp(Child->name, START)) {
+              Start = createFeatureSourceLocation(Child);
+            } else if (!xmlStrcmp(Child->name, END)) {
+              End = createFeatureSourceLocation(Child);
+            }
+          }
+        }
+        Loc = FeatureSourceRange(Path, Start, End);
+      } else if (Num) {
+        if (!xmlStrcmp(Head->name, MINVALUE)) {
+          MinValue = std::stoi(Cnt);
+        } else if (!xmlStrcmp(Head->name, MAXVALUE)) {
+          MaxValue = std::stoi(Cnt);
+        } else if (!xmlStrcmp(Head->name, VALUES)) {
+          const std::regex Regex(R"(\d+)");
+          std::smatch Matches;
+          for (string Suffix = Cnt; regex_search(Suffix, Matches, Regex);
+               Suffix = Matches.suffix()) {
+            Values.emplace_back(std::stoi(Matches.str()));
+          }
+        }
+      }
+    }
+  }
+  if (Num) {
+    if (Values.empty()) {
+      return FMB.makeFeature<NumericFeature>(
+          Name, std::make_pair(MinValue, MaxValue), Opt, std::move(Loc));
+    }
+    return FMB.makeFeature<NumericFeature>(Name, Values, Opt, std::move(Loc));
+  }
+  return FMB.makeFeature<BinaryFeature>(Name, Opt, std::move(Loc));
+}
+
+bool FeatureModelXmlParser::parseOptions(xmlNode *Node, bool Num = false) {
+  for (xmlNode *H = Node->children; H; H = H->next) {
+    if (H->type == XML_ELEMENT_NODE) {
+      if (!xmlStrcmp(H->name, CONFIGURATIONOPTION)) {
+        if (!parseConfigurationOption(H, Num)) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+bool FeatureModelXmlParser::parseConstraints(xmlNode *Node) {
+  for (xmlNode *H = Node->children; H; H = H->next) {
+    if (H->type == XML_ELEMENT_NODE) {
+      if (!xmlStrcmp(H->name, CONSTRAINT)) {
+        string Cnt = std::string(
+            reinterpret_cast<char *>(std::unique_ptr<xmlChar, void (*)(void *)>(
+                                         xmlNodeGetContent(H), xmlFree)
+                                         .get()));
+        const std::regex Regex(R"((!?\w+))");
+        std::smatch Matches;
+        FeatureModel::ConstraintTy Constraint;
+        for (string Suffix = Cnt; regex_search(Suffix, Matches, Regex);
+             Suffix = Matches.suffix()) {
+          string B = Matches.str(0);
+          if (B.length() > 1 && B[0] == '!') {
+            Constraint.emplace_back(B.substr(1, B.length()), false);
+          } else {
+            Constraint.emplace_back(B, true);
+          }
+        }
+        FMB.addConstraint(Constraint);
+      }
+    }
+  }
+  return true;
+}
+
+bool FeatureModelXmlParser::parseVm(xmlNode *Node) {
+  {
+    std::unique_ptr<xmlChar, void (*)(void *)> Cnt(xmlGetProp(Node, NAME),
+                                                   xmlFree);
+    FMB.setVmName(std::string(reinterpret_cast<char *>(Cnt.get())));
+  }
+  {
+    std::unique_ptr<xmlChar, void (*)(void *)> Cnt(xmlGetProp(Node, ROOT),
+                                                   xmlFree);
+    FMB.setPath(Cnt ? fs::path(reinterpret_cast<char *>(Cnt.get()))
+                    : fs::current_path());
+  }
+  for (xmlNode *H = Node->children; H; H = H->next) {
+    if (H->type == XML_ELEMENT_NODE) {
+      if (!xmlStrcmp(H->name, BINARYOPTIONS)) {
+        if (!parseOptions(H)) {
+          return false;
+        }
+      } else if (!xmlStrcmp(H->name, NUMERICOPTIONS)) {
+        if (!parseOptions(H, true)) {
+          return false;
+        }
+      } else if (!xmlStrcmp(H->name, BOOLEANCONSTRAINTS)) {
+        if (!parseConstraints(H)) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+FeatureSourceRange::FeatureSourceLocation
+FeatureModelXmlParser::createFeatureSourceLocation(xmlNode *Node) {
+  int Line = 0;
+  int Column = 0;
+  for (xmlNode *Head = Node->children; Head; Head = Head->next) {
+    if (Head->type == XML_ELEMENT_NODE) {
+      if (!xmlStrcmp(Head->name, LINE)) {
+        Line = atoi(
+            reinterpret_cast<char *>(std::unique_ptr<xmlChar, void (*)(void *)>(
+                                         xmlNodeGetContent(Head), xmlFree)
+                                         .get()));
+      } else if (!xmlStrcmp(Head->name, COLUMN)) {
+        Column = atoi(
+            reinterpret_cast<char *>(std::unique_ptr<xmlChar, void (*)(void *)>(
+                                         xmlNodeGetContent(Head), xmlFree)
+                                         .get()));
+      }
+    }
+  }
+  return FeatureSourceRange::FeatureSourceLocation(Line, Column);
+}
+
+std::unique_ptr<FeatureModel> FeatureModelXmlParser::buildFeatureModel() {
+  auto Doc = parseDoc();
+  if (!Doc) {
+    return nullptr;
+  }
+  FMB.init();
+  return parseVm(xmlDocGetRootElement(Doc.get())) ? FMB.buildFeatureModel()
+                                                  : nullptr;
+}
+
+std::unique_ptr<xmlDtd, void (*)(xmlDtdPtr)>
+FeatureModelXmlParser::createDtd() {
+  std::unique_ptr<xmlDtd, void (*)(xmlDtdPtr)> Dtd(
+      xmlIOParseDTD(nullptr,
+                    xmlParserInputBufferCreateMem(DtdRaw.c_str(),
+                                                  DtdRaw.length(),
+                                                  XML_CHAR_ENCODING_UTF8),
+                    XML_CHAR_ENCODING_UTF8),
+      xmlFreeDtd);
+  xmlCleanupParser();
+  assert(Dtd && "Failed to parse DTD.");
+  return Dtd;
+}
+
+std::unique_ptr<xmlDoc, void (*)(xmlDocPtr)> FeatureModelXmlParser::parseDoc() {
+  std::unique_ptr<xmlParserCtxt, void (*)(xmlParserCtxtPtr)> Ctxt(
+      xmlNewParserCtxt(), xmlFreeParserCtxt);
+  std::unique_ptr<xmlDoc, void (*)(xmlDocPtr)> Doc(
+      xmlCtxtReadMemory(Ctxt.get(), Xml.c_str(), Xml.length(), nullptr, nullptr,
+                        XML_PARSE_NOBLANKS),
+      xmlFreeDoc);
+  xmlCleanupParser();
+  if (Doc && Ctxt->valid) {
+    xmlValidateDtd(&Ctxt->vctxt, Doc.get(), createDtd().get());
+    if (Ctxt->vctxt.valid) {
+      return Doc;
+    } else {
+      std::cerr << "Failed to validate DTD." << std::endl;
+    }
+  } else {
+    std::cerr << "Failed to parse / validate XML." << std::endl;
+  }
+  return std::unique_ptr<xmlDoc, void (*)(xmlDocPtr)>(nullptr, nullptr);
+}
+
+// TODO(s9latimm): replace with builder err
+bool FeatureModelXmlParser::verifyFeatureModel() { return parseDoc().get(); }
+
+} // namespace vara::feature
