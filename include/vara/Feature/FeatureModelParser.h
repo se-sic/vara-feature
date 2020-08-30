ca69970b77fce2d93714a722ca7e26e3ef5c7ac3
padupr <42845342+padupr@users.noreply.github.com>
2020-08-30 20:33:19 +0200

    Adds initial Xmlwriter for FeatureModels (#16)
    Introduces an initial version of the Xmlwriter for FeatureModels, allowing users to store FeatureModels into files.
    
    The initial version is currently lacking some features which will be added later an can be tracked through these issues:
    related se-passau/VaRA#663
    related se-passau/VaRA#664
    related se-passau/VaRA#665
    related se-passau/VaRA#666
    related se-passau/VaRA#667

--- include/vara/Feature/FeatureModelParser.h
+++ include/vara/Feature/FeatureModelParser.h
@@ -49,67 +49,6 @@ public:
 private:
   using constXmlCharPtr = const xmlChar *;
 
-  inline static const std::string DtdRaw =
-      "<!ELEMENT vm (binaryOptions, numericOptions?, booleanConstraints?, "
-      "nonBooleanConstraints?, mixedConstraints?)>\n"
-      "<!ATTLIST vm name CDATA #REQUIRED root CDATA #IMPLIED>\n"
-      "<!ELEMENT binaryOptions (configurationOption*)>\n"
-      "<!ELEMENT numericOptions (configurationOption*)>\n"
-      "<!ELEMENT booleanConstraints (constraint*)>\n"
-      "<!ELEMENT nonBooleanConstraints (constraint*)>\n"
-      "<!ELEMENT mixedConstraints (constraint*)>\n"
-      "<!ELEMENT configurationOption (name, outputString?, (prefix | preFix)?, "
-      "(postfix | postFix)?, parent?, children?, impliedOptions?, "
-      "excludedOptions?, defaultValue?, optional?, ((minValue, maxValue) | "
-      "values)?, "
-      "stepFunction?, location?)>\n"
-      "<!ELEMENT constraint (#PCDATA)>\n"
-      "<!ATTLIST constraint req CDATA #IMPLIED exprKind CDATA #IMPLIED>\n"
-      "<!ELEMENT name (#PCDATA)>\n"
-      "<!ELEMENT outputString (#PCDATA)>\n"
-      "<!ELEMENT prefix (#PCDATA)>\n"
-      "<!ELEMENT preFix (#PCDATA)>\n"
-      "<!ELEMENT postfix (#PCDATA)>\n"
-      "<!ELEMENT postFix (#PCDATA)>\n"
-      "<!ELEMENT parent (#PCDATA)>\n"
-      "<!ELEMENT children (options*)>\n"
-      "<!ELEMENT impliedOptions (options*)>\n"
-      "<!ELEMENT excludedOptions (options*)>\n"
-      "<!ELEMENT defaultValue (#PCDATA)>\n"
-      "<!ELEMENT options (#PCDATA)>\n"
-      "<!ELEMENT optional (#PCDATA)>\n"
-      "<!ELEMENT minValue (#PCDATA)>\n"
-      "<!ELEMENT maxValue (#PCDATA)>\n"
-      "<!ELEMENT values (#PCDATA)>\n"
-      "<!ELEMENT stepFunction (#PCDATA)>\n"
-      "<!ELEMENT location (path, start?, end?)>\n"
-      "<!ELEMENT path (#PCDATA)>\n"
-      "<!ELEMENT start (line, column)>\n"
-      "<!ELEMENT end (line, column)>\n"
-      "<!ELEMENT line (#PCDATA)>\n"
-      "<!ELEMENT column (#PCDATA)>";
-
-  static constexpr xmlChar NAME[] = "name";
-  static constexpr xmlChar OPTIONAL[] = "optional";
-  static constexpr xmlChar PARENT[] = "parent";
-  static constexpr xmlChar EXCLUDEDOPTIONS[] = "excludedOptions";
-  static constexpr xmlChar OPTIONS[] = "options";
-  static constexpr xmlChar LOCATION[] = "location";
-  static constexpr xmlChar PATH[] = "path";
-  static constexpr xmlChar START[] = "start";
-  static constexpr xmlChar END[] = "end";
-  static constexpr xmlChar MINVALUE[] = "minValue";
-  static constexpr xmlChar MAXVALUE[] = "maxValue";
-  static constexpr xmlChar VALUES[] = "values";
-  static constexpr xmlChar CONFIGURATIONOPTION[] = "configurationOption";
-  static constexpr xmlChar CONSTRAINT[] = "constraint";
-  static constexpr xmlChar ROOT[] = "root";
-  static constexpr xmlChar BINARYOPTIONS[] = "binaryOptions";
-  static constexpr xmlChar NUMERICOPTIONS[] = "numericOptions";
-  static constexpr xmlChar BOOLEANCONSTRAINTS[] = "booleanConstraints";
-  static constexpr xmlChar LINE[] = "line";
-  static constexpr xmlChar COLUMN[] = "column";
-
   std::string Xml;
   FeatureModelBuilder FMB;
 
---
+++ include/vara/Feature/FeatureModelWriter.h
@@ -0,0 +1,60 @@
+#ifndef VARA_FEATURE_FEATUREMODELWRITER_H
+#define VARA_FEATURE_FEATUREMODELWRITER_H
+
+#include "vara/Feature/FeatureModel.h"
+
+#include "libxml/tree.h"
+#include "libxml/xmlwriter.h"
+
+#include <memory>
+
+namespace vara::feature {
+
+//===----------------------------------------------------------------------===//
+//                               FeatureModelParser Class
+//===----------------------------------------------------------------------===//
+
+/// \brief Base class for parsers with different input formats.
+class FeatureModelWriter {
+protected:
+  explicit FeatureModelWriter() = default;
+
+public:
+  virtual ~FeatureModelWriter() = default;
+
+  /// Build \a FeatureModel after parsing. May return null if parsing or
+  /// building failed.
+  ///
+  /// \returns an instance of \a FeatureModel or \a nullptr
+  virtual int writeFeatureModel(std::string Path) = 0;
+  virtual std::optional<std::string> writeFeatureModel() = 0;
+
+};
+
+//===----------------------------------------------------------------------===//
+//                               FeatureModelXmlParser Class
+//===----------------------------------------------------------------------===//
+
+/// \brief Parsers for feature models in XML.
+class FeatureModelXmlWriter : public FeatureModelWriter {
+public:
+  explicit FeatureModelXmlWriter(const FeatureModel &Fm) : Fm{Fm}{}
+
+  int writeFeatureModel(std::string Path) override;
+  std::optional<std::string> writeFeatureModel() override;
+
+private:
+  int writeFeatureModel(xmlTextWriterPtr Writer);
+  int writeVm(xmlTextWriterPtr Writer);
+  int writeBinaryFeatures(xmlTextWriterPtr Writer);
+  int writeNumericFeatures(xmlTextWriterPtr Writer);
+  int writeBooleanConstraints(xmlTextWriterPtr Writer);
+  static int writeFeature(xmlTextWriterPtr Writer, Feature &Feature1);
+  static int writeSourceRange(xmlTextWriterPtr Writer, FeatureSourceRange &Location);
+
+  const FeatureModel &Fm;
+};
+
+} // namespace vara::feature
+
+#endif // VARA_FEATURE_FEATUREMODELWRITER_H
