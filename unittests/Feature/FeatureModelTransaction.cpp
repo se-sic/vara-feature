f3cfca7d7d1bdf0f78649457b60a1079223b5229
MiriamMarx <MiriamMarx@users.noreply.github.com>
2021-05-14 14:54:33 +0200

    removing unnecessary move of unique ptr
    Co-authored-by: Florian Sattler <vuld3r@gmail.com>

--- unittests/Feature/FeatureModelTransaction.cpp
+++ unittests/Feature/FeatureModelTransaction.cpp
@@ -852,7 +852,7 @@ TEST_F(FeatureModelTransactionTest, addFeatureToModel) {
   size_t FMSizeBefore = FM->size();
 
   vara::feature::addFeature(*FM,
-                            std::move(std::make_unique<BinaryFeature>("ab")),
+                            std::make_unique<BinaryFeature>("ab"),
                             FM->getFeature("a")); // committed automatically
 
   // Changes should be visible on the new model
