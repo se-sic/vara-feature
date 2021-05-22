2466776a6a38ff1bd6e57a480b41205e8614bfe8
vulder <vulder@users.noreply.github.com>
2021-05-22 12:36:36 +0000

    deploy: a83a0291dc42df7074a198bad5afc137f7978daa

--- search/functions_a.js
+++ search/functions_a.js
@@ -4,7 +4,7 @@ var searchData=
   ['make_5funique_5fmodification_547',['make_unique_modification',['../classvara_1_1feature_1_1detail_1_1FeatureModelModification.html#aea7a496cd0e161a99d733205605d87ae',1,'vara::feature::detail::FeatureModelModification']]],
   ['makefeature_548',['makeFeature',['../classvara_1_1feature_1_1FeatureModelBuilder.html#ae4d2d8c0d4f9a4ebf391acb4c91f9bb8',1,'vara::feature::FeatureModelBuilder']]],
   ['makeroot_549',['makeRoot',['../classvara_1_1feature_1_1FeatureModelBuilder.html#af61469b84e5b448b3d517ae83f321e03',1,'vara::feature::FeatureModelBuilder']]],
-  ['mergefeaturemodels_550',['mergeFeatureModels',['../namespacevara_1_1feature.html#af55e0173e705976a0661042505e8c0e9',1,'vara::feature']]],
-  ['mergesubtree_551',['mergeSubtree',['../namespacevara_1_1feature.html#a741f75fdda24fa92aedbad5e4947353d',1,'vara::feature']]],
+  ['mergefeaturemodels_550',['mergeFeatureModels',['../namespacevara_1_1feature.html#a4dda78a9fd4c0d93131726e05880060d',1,'vara::feature']]],
+  ['mergesubtree_551',['mergeSubtree',['../namespacevara_1_1feature.html#a81ffbe99c6737d7b44cd877e9b08f226',1,'vara::feature']]],
   ['multiplicationconstraint_552',['MultiplicationConstraint',['../classvara_1_1feature_1_1MultiplicationConstraint.html#a9f1dff496c4f8afe069abf0501fa702b',1,'vara::feature::MultiplicationConstraint']]]
 ];
