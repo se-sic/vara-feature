f881bf7f114b78d838ff5d2e9887884f51070d6d
ChristianKaltenecker <kaltenec@cs.uni-saarland.de>
2021-07-15 20:25:31 +0200

    Add documentation for the SXFM format (#58)

--- include/vara/Feature/FeatureModelParser.h
+++ include/vara/Feature/FeatureModelParser.h
@@ -77,8 +77,79 @@ private:
 //===----------------------------------------------------------------------===//
 
 /// \brief Parsers for feature models in SXFM.
-/// The SXFM (simple XML feature model) format is a feature model format embeded
-/// in an XML structure.
+///
+/// \section SXFM
+///
+/// SXFM stands for Simple XML Feature Model and represents a format that is
+/// used to store feature models. In general, feature models consist of a
+/// feature model in a tree-like format and cross-tree constraints (i.e.,
+/// constraints that would violate the tree-like feature). The SXFM format also
+/// distinguishes between a feature tree and cross-tree-constraints. The feature
+/// tree is embeded as a tabulator indented text in an XML tag called
+/// \c `feature_tree` whereas the cross-tree-constraints are embeded in XML xml
+/// tag called \c `constraint`. A brief example and description of the SXFM
+/// format can be found at SPLOT (go to `Edit an existing model` -> `SXFM
+/// format`). However, they have not given a formal description of the syntax in
+/// form of a syntax diagram. Note that the SXFM format does only support binary
+/// features.
+///
+/// \subsection FeatureTree
+///
+/// In the following, we show the syntax diagram for the syntax of feature
+/// trees.
+///
+/// \image html feature_tree.png
+///
+/// The item \c `tabulator` stands for the tabulator symbol.
+///
+/// By using the sequence diagram generator, the syntax diagrams can be
+/// generated when using the following rules:
+///
+/// \code{unparsed}
+/// macro_rules! sxfm_feature_tree {
+///    (:r $name:expr $id:ident) => { ... };
+///    (:r $name:expr) => { ... };
+///    ($tabulator:item $($tabulator:item)* :o $name:expr $id:ident ) => { ...
+///    };
+///    ($tabulator:item $($tabulator:item)* :o $name:expr ) => { ... };
+///    ($tabulator:item $($tabulator:item)* :m $name:expr $id:ident) => { ... };
+///    ($tabulator:item $($tabulator:item)* :m $name:expr ) => { ... };
+///    ($tabulator:item $($tabulator:item)* :g [1..*] ) => { ... };
+///    ($tabulator:item $($tabulator:item)* :g [1..1] ) => { ... };
+///    ($tabulator:item $($tabulator:item)* : $name:expr ) => { ... };
+///    ($tabulator:item $($tabulator:item)* : $name:expr $id:ident ) => { ... };
+///}
+/// \endcode
+///
+/// Note that the tabulators indicate the level of the feature. Only the root
+/// feature is on level 0 (all other features have a higher indentation). There
+/// are different kind of features:
+///
+/// \li \c `:r` the root feature
+/// \li \c `:o` the optional feature
+/// \li \c `:m` the mandatory feature
+/// \li \c `:g` the parent node of an or group
+/// \li \c `:` child of an or group
+///
+/// Note that the or-group features (\c ':g' ) have no name.
+///
+/// \subsection Constraints
+///
+/// \image html constraints.png
+///
+/// or alternatively:
+/// \code{unparsed}
+/// macro_rules! sxfm_constraints {
+///     (:$name:ident $cnf_formula:tt) => {...};
+/// }
+/// \endcode
+///
+/// The constraints have to be provided in CNF format.
+/// Therebey, \c `~` stands for negation, \c `or` for a disjunction.
+/// Each constraint is written in a separate XML tag \c `constraints`.
+///
+/// @see[SPLOT](http://www.splot-research.org/sxfm.html)
+/// @see[SequenceDiagramGenerator](https://lukaslueg.github.io/macro_railroad_wasm_demo/)
 class FeatureModelSxfmParser : public FeatureModelParser {
 public:
   explicit FeatureModelSxfmParser(std::string Sxfm) : Sxfm(std::move(Sxfm)) {}
