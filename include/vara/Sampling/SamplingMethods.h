f75ea07b56193a5b66a85af620e8e5d941b9ff11
Florian Sattler <vuld3r@gmail.com>
2022-01-26 15:25:35 +0100

    Adds initial setup of sampling modules

---
+++ include/vara/Sampling/SamplingMethods.h
@@ -0,0 +1,12 @@
+#ifndef VARA_SAMPLING_SAMPLINGMETHOD_H
+#define VARA_SAMPLING_SAMPLINGMETHOD_H
+
+#include <tuple>
+
+namespace vara::sampling {
+
+std::tuple<double, double, double> getExampleValues();
+
+} // namespace vara::sampling
+
+#endif // VARA_SAMPLING_SAMPLINGMETHOD_H
