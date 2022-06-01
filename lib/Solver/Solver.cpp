ee2024a3e19c03e017f9f19df5d46e8101c9b61d
Christian Kaltenecker <kaltenec@cs.uni-saarland.de>
2022-06-01 17:54:59 +0200

    Add initial test and fix linker

--- lib/Solver/Solver.cpp
+++ lib/Solver/Solver.cpp
@@ -1,5 +1,7 @@
 #include "vara/Solver/Solver.h"
 
+#include "z3++.h"
+
 namespace vara::solver {
 
 } // vara::solver
--- unittests/Solver/BasicSolverTests.cpp
+++ unittests/Solver/BasicSolverTests.cpp
@@ -1,7 +1,25 @@
 #include "vara/Solver/Solver.h"
 
 #include "gtest/gtest.h"
+#include "z3++.h"
 
 namespace vara::solver {
 
-} // vara::solver
+TEST(Solver, Z3BasicTest) {
+  z3::context C;
+
+  z3::expr A = C.bool_const("A");
+  z3::expr B = C.bool_const("B");
+  z3::expr Expression = A && B;
+
+  z3::solver S(C);
+
+  S.add(Expression);
+  EXPECT_EQ(S.check(), z3::check_result::sat);
+
+  z3::expr InvalidatingExpression = !A;
+  S.add(InvalidatingExpression);
+  EXPECT_EQ(S.check(), z3::check_result::unsat);
+}
+
+} // namespace vara::solver
