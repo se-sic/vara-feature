07e4da5d90b18af5fde6115829fa038757d19522
Christian Kaltenecker <kaltenec@cs.uni-saarland.de>
2022-06-01 18:53:18 +0200

    Add header for the Z3 solver

--- include/vara/Solver/Solver.h
+++ include/vara/Solver/Solver.h
@@ -7,7 +7,13 @@
 #include "vara/Solver/Error.h"
 #include "vara/Utils/Result.h"
 
+#include "z3++.h"
+
 namespace vara::solver {
+
+//===----------------------------------------------------------------------===//
+//                               Solver Class
+//===----------------------------------------------------------------------===//
 
 /// \brief This class represents an interface for solvers.
 class Solver {
@@ -56,6 +62,59 @@ public:
   virtual Result<SolverErrorCode,
                  std::vector<std::unique_ptr<vara::feature::Configuration>>>
   getAllValidConfigurations() = 0;
+};
+
+//===----------------------------------------------------------------------===//
+//                               Z3Solver Class
+//===----------------------------------------------------------------------===//
+
+/// \brief The Z3 solver implementation for handling constraints
+class Z3Solver : public Solver {
+public:
+  explicit Z3Solver() : Solver(Context) {}
+
+  Result<SolverErrorCode>
+  addFeature(const feature::Feature &FeatureToAdd) override;
+
+  Result<SolverErrorCode> addFeature(string FeatureName) override;
+
+  Result<SolverErrorCode>
+  addFeature(const string &FeatureName,
+             const std::vector<int64_t> &Values) override;
+
+  Result<SolverErrorCode>
+  removeFeature(const feature::Feature &FeatureToRemove) override;
+
+  Result<SolverErrorCode>
+  addConstraint(const feature::Constraint &ConstraintToAdd) override;
+
+  Result<SolverErrorCode>
+  removeConstraint(const feature::Constraint &ConstraintToRemove) override;
+
+  Result<SolverErrorCode>
+  minimizeConfiguration(const feature::Configuration &Config) override;
+
+  Result<SolverErrorCode>
+  maximizeConfiguration(const feature::Configuration &Config) override;
+
+  bool hasValidConfigurations() override;
+
+  Result<SolverErrorCode, u_int64_t> getNumberValidConfigurations() override;
+
+  Result<SolverErrorCode, std::unique_ptr<vara::feature::Configuration>>
+  getNextConfiguration() override;
+
+  Result<SolverErrorCode,
+         std::vector<std::unique_ptr<vara::feature::Configuration>>>
+  getAllValidConfigurations() override;
+
+private:
+  /// The context of Z3 needed for every allocation.
+  z3::context Context;
+
+  /// The instance of the Z3 solver needed for caching the constraints and
+  /// variables.
+  z3::solver Solver;
 };
 
 } // namespace vara::solver
