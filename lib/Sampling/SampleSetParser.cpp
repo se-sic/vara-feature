05c482d006f2cc52e8ba4c66b1c8c0a07a5abaac
Christian Kaltenecker <kaltenec@cs.uni-saarland.de>
2023-01-16 16:33:42 +0100

    Enhance test

--- lib/Sampling/SampleSetParser.cpp
+++ lib/Sampling/SampleSetParser.cpp
@@ -12,14 +12,23 @@ SampleSetParser::readConfigurations(const feature::FeatureModel &Model,
   auto V = std::make_unique<
       std::vector<std::unique_ptr<vara::feature::Configuration>>>();
   csv::CSVReader Reader(CsvPath, Format);
-  for (const csv::CSVRow &Row : Reader) {
+  for (const csv::CSVRow &Row: Reader) {
     auto Config = std::make_unique<vara::feature::Configuration>();
     auto ColumnNames = Row.get_col_names();
-    for (auto *F : Model.features()) {
+    for (auto *F: Model.features()) {
       if (std::find(ColumnNames.begin(), ColumnNames.end(), F->getName()) !=
           ColumnNames.end()) {
-        Config->setConfigurationOption(
-            F->getName(), llvm::StringRef(Row[F->getName().str()]));
+        if (F->getKind() == feature::Feature::FeatureKind::FK_BINARY ||
+            F->getKind() == feature::Feature::FeatureKind::FK_ROOT) {
+          if (std::string(Row[F->getName().str()].get()) == "0") {
+            Config->setConfigurationOption(F->getName(), "false");
+          } else {
+            Config->setConfigurationOption(F->getName(), "true");
+          }
+        } else {
+          Config->setConfigurationOption(
+              F->getName(), llvm::StringRef(Row[F->getName().str()].get()));
+        }
       }
     }
     V->insert(V->begin(), std::move(Config));
--- unittests/Sampling/SampleSetParserTests.cpp
+++ unittests/Sampling/SampleSetParserTests.cpp
@@ -9,6 +9,14 @@ inline std::string getTestResource(llvm::StringRef ResourcePath = "") {
   return (llvm::Twine(BasePath) + ResourcePath).str();
 }
 
+inline void testConfigurationOption(const std::unique_ptr<vara::feature::Configuration> &Config,
+                                    llvm::StringRef Feature,
+                                    llvm::StringRef ExpectedValue) {
+  auto Value = Config->configurationOptionValue(Feature);
+  EXPECT_TRUE(Value);
+  EXPECT_EQ(Value.getValue(), ExpectedValue.str());
+}
+
 namespace vara::sampling {
 TEST(SampleSetParser, testCSVParsing) {
   auto CsvPath = getTestResource("configs_dune.csv");
@@ -19,7 +27,34 @@ TEST(SampleSetParser, testCSVParsing) {
   auto P = vara::feature::FeatureModelXmlParser(FMFS.get()->getBuffer().str());
   auto FM = P.buildFeatureModel();
   auto Configs = SampleSetParser::readConfigurations(*FM, CsvPath);
-  std::cout << "Size: " << Configs->size() << "\n";
   EXPECT_EQ(Configs->size(), 40);
+  // Check the configuration options of the first configuration
+  const auto &FirstConfig = Configs->back();
+  testConfigurationOption(FirstConfig, "root", "true");
+  testConfigurationOption(FirstConfig, "Precon", "true");
+  testConfigurationOption(FirstConfig, "Solver", "true");
+  testConfigurationOption(FirstConfig, "SeqGS", "true");
+  testConfigurationOption(FirstConfig, "SeqSOR", "false");
+  testConfigurationOption(FirstConfig, "CGSolver", "false");
+  testConfigurationOption(FirstConfig, "BiCGSTABSolver", "false");
+  testConfigurationOption(FirstConfig, "LoopSolver", "false");
+  testConfigurationOption(FirstConfig, "GradientSolver", "true");
+  testConfigurationOption(FirstConfig, "pre", "0");
+  testConfigurationOption(FirstConfig, "post", "3");
+  testConfigurationOption(FirstConfig, "cells", "55");
+  // Check the configuration options of the last configuration
+  const auto &LastConfig = Configs->front();
+  testConfigurationOption(LastConfig, "root", "true");
+  testConfigurationOption(LastConfig, "Precon", "true");
+  testConfigurationOption(LastConfig, "Solver", "true");
+  testConfigurationOption(LastConfig, "SeqGS", "false");
+  testConfigurationOption(LastConfig, "SeqSOR", "true");
+  testConfigurationOption(LastConfig, "CGSolver", "false");
+  testConfigurationOption(LastConfig, "BiCGSTABSolver", "false");
+  testConfigurationOption(LastConfig, "LoopSolver", "true");
+  testConfigurationOption(LastConfig, "GradientSolver", "false");
+  testConfigurationOption(LastConfig, "pre", "3");
+  testConfigurationOption(LastConfig, "post", "0");
+  testConfigurationOption(LastConfig, "cells", "52");
 }
 } // namespace vara::sampling
