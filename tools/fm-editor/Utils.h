e01fd2f3c89718c046b350bca9b0b38b2afe61ff
Simon <s8sifrie@stud.uni-saarland.de>
2023-02-22 14:57:30 +0100

    Apply suggestions from formater
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/fm-editor/Utils.h
+++ tools/fm-editor/Utils.h
@@ -6,30 +6,30 @@
 #define VARA_FEATURE_UTILS_H
 
 #include <QMenu>
-template<class T>
-struct ActionBuilder{
-  ActionBuilder(QMenu* Menu,T* Receiver) : Menu(Menu), Receiver(Receiver) {}
-  template <class Func1,class ...Func>
-  void addActions(std::pair<QString,Func1> Action,std::pair<QString,Func>... Actions) {
-    auto *A = new QAction(Action.first,Receiver);
-    QObject::connect(A,&QAction::triggered,Receiver,Action.second);
+template <class T>
+struct ActionBuilder {
+  ActionBuilder(QMenu *Menu, T *Receiver) : Menu(Menu), Receiver(Receiver) {}
+  template <class Func1, class... Func>
+  void addActions(std::pair<QString, Func1> Action,
+                  std::pair<QString, Func>... Actions) {
+    auto *A = new QAction(Action.first, Receiver);
+    QObject::connect(A, &QAction::triggered, Receiver, Action.second);
     Menu->addAction(A);
     addActions(Actions...);
   }
   void addActions() {}
+
 private:
-  QMenu* Menu;
-  T* Receiver;
+  QMenu *Menu;
+  T *Receiver;
 };
 
-
-template<class T,class ...Func>
-QMenu* buildMenu(T* Receiver,std::pair<QString,Func>... Actions ) {
+template <class T, class... Func>
+QMenu *buildMenu(T *Receiver, std::pair<QString, Func>... Actions) {
   auto Menu = new QMenu;
-  ActionBuilder Builder(Menu,Receiver);
+  ActionBuilder Builder(Menu, Receiver);
   Builder.addActions(Actions...);
   return Menu;
 }
-
 
 #endif // VARA_FEATURE_UTILS_H
