2d8f417861faaa109cb8fa186ad0b670e1c382fd
Christian Kaltenecker <kaltenec@cs.uni-saarland.de>
2023-03-18 22:02:53 +0100

    Implement configuration generator

--- tools/config-generator/ConfigurationGenerator.cpp
+++ tools/config-generator/ConfigurationGenerator.cpp
@@ -1,48 +1,57 @@
 #include "vara/Feature/FeatureModel.h"
+#include "vara/Sampling/SampleSetParser.h"
+#include "vara/Sampling/SampleSetWriter.h"
+#include "vara/Solver/ConfigurationFactory.h"
 
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Errc.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/Program.h"
 
 static llvm::cl::OptionCategory
     ConfigCreatorCategory("Configuration generator options");
 
-static llvm::cl::list<std::string> FileNames(llvm::cl::Positional,
-                                             llvm::cl::desc("file"),
-                                             llvm::cl::cat(FMViewerCategory));
+static llvm::cl::list<std::string>
+    FileNames(llvm::cl::Positional, llvm::cl::desc("Path to the feature model"),
+              llvm::cl::cat(ConfigCreatorCategory));
 
-static llvm::cl::opt<bool> Xml("xml",
-                               llvm::cl::desc("Use XML format (default)."),
-                               llvm::cl::init(true),
-                               llvm::cl::cat(FMViewerCategory));
+enum class ConfigurationGenerationChoice : unsigned {
+  ALL,
+  SAMPLING_STRATEGY,
+  SAMPLE_SET,
+};
 
-static llvm::cl::opt<bool> Verify("verify",
-                                  llvm::cl::desc("Verify input and exit."),
-                                  llvm::cl::init(false),
-                                  llvm::cl::cat(FMViewerCategory));
+static llvm::cl::opt<ConfigurationGenerationChoice, false>
+    ConfigurationGenerationOption(
+        "config-type",
+        llvm::cl::desc("The way how the configurations should be generated."),
+        llvm::cl::values(
+            clEnumValN(ConfigurationGenerationChoice::ALL, "All",
+                       "Generate all configurations from feature model."),
+            clEnumValN(ConfigurationGenerationChoice::SAMPLING_STRATEGY,
+                       "sampling",
+                       "Use a sampling strategy to select configurations."),
+            clEnumValN(ConfigurationGenerationChoice::SAMPLE_SET, "sample",
+                       "Read the sample set from a file.")),
+        llvm::cl::init(ConfigurationGenerationChoice::ALL), llvm::cl::Optional,
+        llvm::cl::cat(ConfigCreatorCategory));
 
 static llvm::cl::opt<std::string>
-    Viewer("viewer", llvm::cl::desc("Use <executable> to view DOT file."),
-           llvm::cl::value_desc("executable"), llvm::cl::init(""),
-           llvm::cl::cat(FMViewerCategory));
+    CsvInputFilePath("csv", llvm::cl::desc("Path to the csv input file."),
+                     llvm::cl::value_desc("filename"), llvm::cl::init(""),
+                     llvm::cl::cat(ConfigCreatorCategory));
 
 static llvm::cl::opt<std::string>
-    Out("o", llvm::cl::desc("Place DOT code into <file>, disables viewer."),
-        llvm::cl::value_desc("file"), llvm::cl::init(""),
-        llvm::cl::cat(FMViewerCategory));
-
-static llvm::cl::opt<bool>
-    Dump("dump", llvm::cl::desc("Dump feature model to stdout and exit."),
-         llvm::cl::init(false), llvm::cl::cat(FMViewerCategory));
+    OutputFilePath("out", llvm::cl::desc("Path to the yml output file."),
+                   llvm::cl::value_desc("filename"), llvm::cl::init(""),
+                   llvm::cl::cat(ConfigCreatorCategory));
 
 int main(int Argc, char **Argv) {
-  llvm::InitLLVM X(Argc, Argv);
-  llvm::cl::HideUnrelatedOptions(FMViewerCategory);
+  const llvm::InitLLVM X(Argc, Argv);
+  llvm::cl::HideUnrelatedOptions(ConfigCreatorCategory);
 
-  const char *FlagsEnvVar = "FM_VIEWER_FLAGS";
-  const char *Overview = R"(View feature model as graph.)";
+  const char *FlagsEnvVar = "CONFIG_GENERATOR_FLAGS";
+  const char *Overview = R"(Generate configurations.)";
 
   llvm::cl::ParseCommandLineOptions(Argc, Argv, Overview, nullptr, FlagsEnvVar);
   if (FileNames.size() != 1) {
@@ -50,7 +59,7 @@ int main(int Argc, char **Argv) {
     return 1;
   }
 
-  if (Verify && !vara::feature::verifyFeatureModel(FileNames[0])) {
+  if (!vara::feature::verifyFeatureModel(FileNames[0])) {
     llvm::errs() << "error: Invalid feature model.\n";
     return 1;
   }
@@ -63,29 +72,47 @@ int main(int Argc, char **Argv) {
     return 1;
   }
 
-  if (Dump) {
-    FM->dump();
-  } else if (!Out.empty()) {
-    llvm::errs() << "Writing '" << Out << "'...";
-    llvm::WriteGraph(FM.get(), llvm::Twine(FM->getName()), false, "",
-                     Out.getValue());
-  } else {
-    auto Filename = llvm::WriteGraph(FM.get(), llvm::Twine(FM->getName()));
+  if (ConfigurationGenerationOption.getValue() ==
+          ConfigurationGenerationChoice::SAMPLE_SET &&
+      CsvInputFilePath.empty()) {
+    llvm::errs()
+        << "error: Please provide the sample set using the csv argument.\n";
+    return 1;
+  }
 
-    if (Filename.empty()) {
+  std::vector<std::unique_ptr<vara::feature::Configuration>> Configurations;
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> FS = nullptr;
+  switch (ConfigurationGenerationOption.getValue()) {
+  case ConfigurationGenerationChoice::ALL:
+    if (auto R = vara::solver::ConfigurationFactory::getAllConfigs(*FM); R) {
+      Configurations = R.extractValue();
+    } else {
+      llvm::errs() << "error: Error while computing all configurations.\n";
       return 1;
     }
+    break;
+  case ConfigurationGenerationChoice::SAMPLE_SET:
+    FS = llvm::MemoryBuffer::getFileAsStream(CsvInputFilePath);
+    assert(FS);
+    Configurations = vara::sampling::SampleSetParser::readConfigurations(
+        *FM, FS.get()->getBuffer().str());
+    break;
+  case ConfigurationGenerationChoice::SAMPLING_STRATEGY:
+    llvm::errs() << "error: Sampling is not implemented yet.\n";
+    return 1;
+  }
 
-    if (llvm::ErrorOr<std::string> P =
-            Viewer.empty() ? llvm::errc::invalid_argument
-                           : llvm::sys::findProgramByName(Viewer)) {
-      llvm::errs() << "Trying '" << *P << "' program... \n";
-      llvm::sys::ExecuteNoWait(*P, {*P, Filename}, llvm::None);
-    } else {
-      llvm::DisplayGraph(Filename);
+  if (!OutputFilePath.empty() && !Configurations.empty()) {
+    const std::string Str = vara::sampling::SampleSetWriter::writeConfigurations(
+        *FM, Configurations);
+    std::error_code EC;
+    auto *Out = new llvm::raw_fd_ostream(OutputFilePath.getValue(), EC);
+    if (EC) {
+      llvm::errs() << "error: Error while writing to file.\n";
+      return 1;
     }
-
-    llvm::errs() << "Remember to erase graph file: \'" << Filename << "\'\n";
+    *Out << Str;
+    delete Out;
   }
 
   return 0;
