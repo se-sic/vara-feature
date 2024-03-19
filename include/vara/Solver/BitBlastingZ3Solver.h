432c98e189ef8eed1603898e8138e3707e452489
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-03-19 16:40:22 +0100

    Add initial support for a bit-blasting encoding of numerical features.

---
+++ include/vara/Solver/BitBlastingZ3Solver.h
@@ -0,0 +1,101 @@
+#ifndef VARA_SOLVER_BITBLASTINGZ3SOLVER_H_
+#define VARA_SOLVER_BITBLASTINGZ3SOLVER_H_
+
+#include "vara/Solver/Solver.h"
+
+#include <z3++.h>
+
+namespace vara::solver {
+//===----------------------------------------------------------------------===//
+//                         BitBlastingZ3Solver Class
+//===----------------------------------------------------------------------===//
+
+class FeatureEncoder {
+public:
+  explicit FeatureEncoder(const feature::Feature &F) : F(&F) {}
+  virtual ~FeatureEncoder() = default;
+
+  virtual std::unique_ptr<z3::expr>
+  createSolverVariable(z3::context &Context) = 0;
+
+  virtual void
+  addConstraintsToSolver(z3::solver *Solver,
+                         llvm::StringMap<z3::expr *> &FeatureVariables) = 0;
+
+protected:
+  const feature::Feature *F;
+};
+
+class BitBlastingZ3Solver final : public Solver {
+public:
+  explicit BitBlastingZ3Solver(z3::config &Cfg)
+      : Context(Cfg), Solver(z3::solver(Context)) {}
+
+  static std::unique_ptr<BitBlastingZ3Solver> create() {
+    // The configuration of z3
+    z3::config Cfg;
+    Cfg.set("model", "true");
+    Cfg.set("model-validate", "true");
+    return std::make_unique<BitBlastingZ3Solver>(Cfg);
+  }
+
+  Result<SolverErrorCode>
+  addFeature(const feature::Feature &FeatureToAdd,
+             bool IsInAlternativeGroup = false) override;
+
+  Result<SolverErrorCode> addFeature(const string &FeatureName) override {
+    return NOT_SUPPORTED;
+  }
+
+  Result<SolverErrorCode>
+  addFeature(const string &FeatureName,
+             const std::vector<int64_t> &Values) override {
+    return NOT_SUPPORTED;
+  }
+
+  Result<SolverErrorCode>
+  removeFeature(feature::Feature &FeatureToRemove) override {
+    return NOT_SUPPORTED;
+  }
+
+  Result<SolverErrorCode>
+  addRelationship(const feature::Relationship &R) override {
+    return NOT_IMPLEMENTED;
+  }
+
+  Result<SolverErrorCode>
+  addConstraint(feature::Constraint &ConstraintToAdd) override {
+    return NOT_IMPLEMENTED;
+  }
+
+  Result<SolverErrorCode>
+  addMixedConstraint(feature::Constraint &ConstraintToAdd,
+                     feature::FeatureModel::MixedConstraint::ExprKind ExprKind,
+                     feature::FeatureModel::MixedConstraint::Req Req) override {
+    return NOT_IMPLEMENTED;
+  }
+
+  Result<SolverErrorCode, bool> hasValidConfigurations() override {
+    return Error(NOT_IMPLEMENTED);
+  }
+
+  Result<SolverErrorCode, std::unique_ptr<vara::feature::Configuration>>
+  getCurrentConfiguration() override {
+    return Error(NOT_IMPLEMENTED);
+  }
+
+  Result<SolverErrorCode, std::unique_ptr<vara::feature::Configuration>>
+  getNextConfiguration() override {
+    return Error(NOT_IMPLEMENTED);
+  }
+
+private:
+  z3::context Context;
+  z3::solver Solver;
+
+  llvm::StringMap<std::unique_ptr<FeatureEncoder>> EncodedFeatures;
+};
+
+} // namespace vara::solver
+
+#endif // VARA_SOLVER_BITBLASTINGZ3SOLVER_H_
---
+++ lib/Solver/BitBlastingZ3Solver.cpp
@@ -0,0 +1,142 @@
+#include "vara/Solver/BitBlastingZ3Solver.h"
+
+namespace vara::solver {
+
+using namespace vara::feature;
+
+class RootEncoder : public FeatureEncoder {
+public:
+  explicit RootEncoder(const Feature &F) : FeatureEncoder(F) {}
+
+  std::unique_ptr<z3::expr>
+  createSolverVariable(z3::context &Context) override {
+    return std::make_unique<z3::expr>(
+        Context.bool_const(F->getName().str().c_str()));
+  }
+
+  void addConstraintsToSolver(
+      z3::solver *Solver,
+      llvm::StringMap<z3::expr *> &FeatureVariables) override {}
+};
+
+class BinaryEncoder : public FeatureEncoder {
+public:
+  explicit BinaryEncoder(const Feature &F, const bool IsInAlternativeGroup)
+      : FeatureEncoder(F), IsInAlternativeGroup(IsInAlternativeGroup) {}
+
+  std::unique_ptr<z3::expr>
+  createSolverVariable(z3::context &Context) override {
+    return std::make_unique<z3::expr>(
+        Context.bool_const(F->getName().str().c_str()));
+  }
+
+  void addConstraintsToSolver(
+      z3::solver *Solver,
+      llvm::StringMap<z3::expr *> &FeatureVariables) override {
+    const auto *Var = FeatureVariables[F->getName()];
+    const auto *ParentVar = FeatureVariables[F->getParentFeature()->getName()];
+    Solver->add(z3::implies(*Var, *ParentVar));
+
+    if (!IsInAlternativeGroup && !F->isOptional()) {
+      Solver->add(z3::implies(*ParentVar, *Var));
+    }
+  }
+
+private:
+  bool IsInAlternativeGroup;
+};
+
+class ListEncoder : public FeatureEncoder {
+public:
+  explicit ListEncoder(const Feature &F,
+                       const NumericFeature::ValueListType &ValueList)
+      : FeatureEncoder(F) {}
+
+  std::unique_ptr<z3::expr>
+  createSolverVariable(z3::context &Context) override {
+    assert(false); // "Not implemented!"
+  }
+
+  void addConstraintsToSolver(
+      z3::solver *Solver,
+      llvm::StringMap<z3::expr *> &FeatureVariables) override {
+    assert(false); // "Not implemented!"
+  }
+
+private:
+};
+
+class RangeEncoder : public FeatureEncoder {
+public:
+  explicit RangeEncoder(const Feature &F,
+                        const NumericFeature::ValueRangeType &ValueRange)
+      : FeatureEncoder(F) {
+    const bool IsSigned = ValueRange.first < 0;
+    BitLength = std::ceil(std::log2(std::max(std::abs(ValueRange.first),
+                                             std::abs(ValueRange.first))) +
+                          1);
+    if (IsSigned) {
+      BitLength += 1;
+    }
+  }
+
+  std::unique_ptr<z3::expr>
+  createSolverVariable(z3::context &Context) override {
+    return std::make_unique<z3::expr>(
+        Context.bv_const(F->getName().str().c_str(), BitLength));
+  }
+
+  void addConstraintsToSolver(
+      z3::solver *Solver,
+      llvm::StringMap<z3::expr *> &FeatureVariables) override {
+    assert(false); // "Not implemented!"
+  }
+
+private:
+  uint16_t BitLength;
+};
+
+Result<SolverErrorCode>
+BitBlastingZ3Solver::addFeature(const Feature &FeatureToAdd,
+                                bool IsInAlternativeGroup) {
+  const auto *Parent = FeatureToAdd.getParentFeature();
+  if (Parent != nullptr &&
+      EncodedFeatures.find(Parent->getName()) == EncodedFeatures.end()) {
+    return PARENT_NOT_PRESENT;
+  }
+
+  if (EncodedFeatures.find(FeatureToAdd.getName()) != EncodedFeatures.end()) {
+    return ALREADY_PRESENT;
+  }
+
+  const auto FeatureName = FeatureToAdd.getName().str();
+
+  if (const auto *NumericFeature =
+          llvm::dyn_cast<feature::NumericFeature>(&FeatureToAdd)) {
+    const auto Values = NumericFeature->getValues();
+    if (const auto *ValueList =
+            std::get_if<NumericFeature::ValueListType>(&Values)) {
+      EncodedFeatures.try_emplace(
+          FeatureName, std::make_unique<ListEncoder>(FeatureToAdd, *ValueList));
+    }
+    if (const auto *ValueRange =
+            std::get_if<NumericFeature::ValueRangeType>(&Values)) {
+      EncodedFeatures.try_emplace(FeatureName, std::make_unique<RangeEncoder>(
+                                                   FeatureToAdd, *ValueRange));
+    }
+  }
+  if (llvm::isa<BinaryFeature>(&FeatureToAdd)) {
+    EncodedFeatures.try_emplace(
+        FeatureName,
+        std::make_unique<BinaryEncoder>(FeatureToAdd, IsInAlternativeGroup));
+    return Ok();
+  }
+  if (llvm::isa<RootFeature>(&FeatureToAdd)) {
+    EncodedFeatures.try_emplace(FeatureName,
+                                std::make_unique<RootEncoder>(FeatureToAdd));
+    return Ok();
+  }
+  return NOT_SUPPORTED;
+}
+
+} // namespace vara::solver
