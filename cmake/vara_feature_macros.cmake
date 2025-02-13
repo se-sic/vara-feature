958f0c0d23df807b0e56fe8914f144aba55de870
Florian Sattler <vuld3r@gmail.com>
2025-02-13 08:56:40 +0100

    Update CI setup to run a modern compiler in addition
    In addition to out checks for older compilers (to support older
    systems), we should also use a new compiler to profit from extra checks
    and ensure we stay compatible to updated compiler versions.

--- cmake/vara_feature_macros.cmake
+++ cmake/vara_feature_macros.cmake
@@ -97,8 +97,9 @@ endif()
 function(check_std_filesystems varname)
   set(old_cmake_required_flags ${CMAKE_REQUIRED_FLAGS})
   set(CMAKE_REQUIRED_FLAGS "-std=c++17 ${CMAKE_REQUIRED_FLAGS}")
-  check_cxx_source_compiles(
-    "
+  set(CMAKE_REQUIRED_QUIET FALSE)
+  message(STATUS "Args: ${CMAKE_REQUIRED_FLAGS}")
+  check_cxx_source_compiles("
 #include <filesystem>
 namespace fs = std::filesystem;
 int main() { return 0; }
