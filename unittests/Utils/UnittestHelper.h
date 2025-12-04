069102a1e2ff9d0f1e05e571acd7ab5a22b994d9
Nerum <nerumlp@gmail.com>
2025-12-04 14:45:06 +0100

    fix test for uvl parser

--- unittests/Utils/UnittestHelper.h
+++ unittests/Utils/UnittestHelper.h
@@ -4,18 +4,9 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Twine.h"
 
-// Allow probing the filesystem to find resources from different working
-// directories (build dir, repo root, etc.).
-#include <filesystem>
-#include <string>
-#include <vector>
-
 inline std::string getTestResource(llvm::StringRef ResourcePath = "") {
-  auto file_type = std::filesystem::path(ResourcePath.str()).extension();
   constexpr const char *BasePath = "resources/";
-  return (llvm::Twine(BasePath) + "/" + string(file_type).substr(1) + "/" +
-          ResourcePath)
-      .str();
+  return (llvm::Twine(BasePath) + ResourcePath).str();
 }
 
 namespace vara::feature {
